277fcb5623eb688736a27db7e0612aa195319c4b
Hungitdev <hungitdev@gmail.com>
2025-10-15 16:19:13 -0600

    add view page for category and item

--- routes/categoriesRouter.js
+++ routes/categoriesRouter.js
@@ -3,4 +3,7 @@ const categoriesRouter = Router();
 categoriesRouter.get("/create", (req, res) => {
   res.render("create-category-page");
 });
+categoriesRouter.get("/1",(req,res)=>{
+  res.render("category-view");
+})
 module.exports = categoriesRouter;
--- routes/itemsRouter.js
+++ routes/itemsRouter.js
@@ -3,4 +3,7 @@ const itemsRouter = Router();
 itemsRouter.get("/create", (req, res) => {
   res.render("create-item-page");
 });
+itemsRouter.get("/1",(req,res)=>{
+  res.render("item-view")
+})
 module.exports = itemsRouter;
