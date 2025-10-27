773dd943685d05926ca8b807610b433a03e2c105
hungit-dev <hungitdev@gmail.com>
2025-10-27 12:30:19 -0600

    add try/catch block to all functions in controllers to handle errors

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -1,9 +1,10 @@
 const db = require("../db/queries");
 const { body, validationResult } = require("express-validator");
+
 async function showItemsInCategoryGet(req, res) {
   //show all items in 1 category
   try {
-    category_id = Number(req.params.id);
+    const category_id = Number(req.params.id);
     const data = await db.getItemsForCategory(category_id);
     const categoryName = data[0]["category_name"];
     let categoryInfo = {};
@@ -27,58 +28,88 @@ async function showItemsInCategoryGet(req, res) {
     });
   } catch (err) {
     console.error(err);
-    res.status(500).send("Sever error");
+    res.status(500).send("Server error");
   }
 }
+
 const validateCategoryForm = [
   body("category-name").trim().notEmpty().withMessage("Name cannot be empty."),
 ];
+
 async function addNewCategoryPost(req, res) {
-  //add new category
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res
-      .status(400)
-      .render("create-category-page", { errors: errors.array() });
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res
+        .status(400)
+        .render("create-category-page", { errors: errors.array() });
+    }
+    const categoryRows = await db.getCategoryIdByName(
+      req.body["category-name"]
+    );
+    if (categoryRows.length > 0) {
+      //if the category already exists, show error
+      return res.render("create-category-page", {
+        errors: [{ msg: "Category already exists" }],
+      });
+    }
+    await db.addNewCategory(req.body["category-name"]);
+    res.redirect("/");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
   }
-  const categoryRows = await db.getCategoryIdByName(req.body["category-name"]);
-  if (categoryRows.length > 0) {
-    //if the category already exists, show error
-    res.render("create-category-page", {
-      errors: [{ msg: "Category already exists" }],
-    });
-    return;
-  }
-  await db.addNewCategory(req.body["category-name"]);
-  res.redirect("/");
 }
+
 async function removeCategoryGet(req, res) {
-  const id = req.params.id;
-  await db.removeCategory(id);
-  res.redirect("/");
-}
-async function showFormToEditGet(req,res) {
-  const categoryNameRows = await db.searchCategoryNameById(req.params.id)
-  const categoryName=categoryNameRows[0].category_name
-  res.render('edit-category-page',{errors:[],categoryName:categoryName,id:req.params.id})
-}
-async function editCategoryNamePost(req,res){
-  const categoryId=req.params.id
-  const newName=req.body["category-name"]
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res
-      .status(400)
-      .render("create-category-page", { errors: errors.array() });
+  try {
+    const id = req.params.id;
+    await db.removeCategory(id);
+    res.redirect("/");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
   }
-  await db.editCategoryName(newName,categoryId)
-  res.redirect("/")
 }
+
+async function showFormToEditGet(req, res) {
+  try {
+    const categoryNameRows = await db.searchCategoryNameById(req.params.id);
+    const categoryName = categoryNameRows[0].category_name;
+    res.render("edit-category-page", {
+      errors: [],
+      categoryName: categoryName,
+      id: req.params.id,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+  }
+}
+
+async function editCategoryNamePost(req, res) {
+  try {
+    const categoryId = req.params.id;
+    const newName = req.body["category-name"];
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res
+        .status(400)
+        .render("create-category-page", { errors: errors.array() });
+    }
+    await db.editCategoryName(newName, categoryId);
+    res.redirect("/");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+  }
+}
+
 module.exports = {
   showItemsInCategoryGet,
   validateCategoryForm,
   addNewCategoryPost,
   removeCategoryGet,
   showFormToEditGet,
-  editCategoryNamePost
+  editCategoryNamePost,
 };
--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -1,5 +1,6 @@
 const db = require("../db/queries");
 const { body, validationResult } = require("express-validator");
+
 const validateItemForm = [
   body("item-name").trim().notEmpty().withMessage("Name cannot be empty."),
   body("quantity")
@@ -9,85 +10,109 @@ const validateItemForm = [
     .isInt({ min: 1 })
     .withMessage("Quantity must be a positive number"),
 ];
+
 async function showCreateItemPageGet(req, res) {
-  //get all categories and show them as options in create item form
-  const data = await db.showAllCategoryNames();
-  let categoryNames = [];
-  for (let category of data) {
-    categoryNames.push(category["category_name"]);
+  try {
+    //get all categories and show them as options in create item form
+    const data = await db.showAllCategoryNames();
+    let categoryNames = [];
+    for (let category of data) {
+      categoryNames.push(category["category_name"]);
+    }
+    res.render("create-item-page", { categories: categoryNames, errors: [] });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
   }
-  res.render("create-item-page", { categories: categoryNames, errors: [] });
 }
+
 async function addNewItemToCategoryPost(req, res) {
-  //add new item to a category
-  const data = await db.showAllCategoryNames();
-  let categoryNames = [];
-  for (let category of data) {
-    categoryNames.push(category["category_name"]);
-  }
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).render("create-item-page", {
-      categories: categoryNames,
-      errors: errors.array(),
-    });
-  }
-  //get item_id to add to create new order
-  const itemNameData = await db.searchItem(req.body["item-name"]);
-  if (itemNameData.length === 0) {
-    //run if item which user is trying to add does not exist
-    const newItem = await db.addNewItem(req.body["item-name"]);
-  }
-  const itemRows = await db.getItemIdByName(req.body["item-name"]);
-  const itemId = Number(itemRows[0].item_id);
+  try {
+    const data = await db.showAllCategoryNames();
+    let categoryNames = [];
+    for (let category of data) {
+      categoryNames.push(category["category_name"]);
+    }
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render("create-item-page", {
+        categories: categoryNames,
+        errors: errors.array(),
+      });
+    }
+    //get item_id to add to create new order
+    const itemNameData = await db.searchItem(req.body["item-name"]);
+    if (itemNameData.length === 0) {
+      //run if item which user is trying to add does not exist
+      await db.addNewItem(req.body["item-name"]);
+    }
+    const itemRows = await db.getItemIdByName(req.body["item-name"]);
+    const itemId = Number(itemRows[0].item_id);
 
-  //get category_id to create new order
-  const categoryRows = await db.getCategoryIdByName(req.body["category"]);
-  const categoryId = Number(categoryRows[0].category_id);
+    //get category_id to create new order
+    const categoryRows = await db.getCategoryIdByName(req.body["category"]);
+    const categoryId = Number(categoryRows[0].category_id);
 
-  //get quantity to create new order
-  const quantity = Number(req.body["quantity"]);
-  const orderRows = await db.selectOrderByItemIdAndCategoryId(
-    itemId,
-    categoryId
-  );
-  if (orderRows.length === 0) {
-    //check if item not exists in the category
-    await db.addNewOrder(itemId, categoryId, quantity);
-  } else {
-    res.render("create-item-page", {
-      categories: categoryNames,
-      errors: [{ msg: "Item  already exists in the category" }],
-    }); // if exists, alert user
-    return;
+    //get quantity to create new order
+    const quantity = Number(req.body["quantity"]);
+    const orderRows = await db.selectOrderByItemIdAndCategoryId(
+      itemId,
+      categoryId
+    );
+    if (orderRows.length === 0) {
+      //check if item not exists in the category
+      await db.addNewOrder(itemId, categoryId, quantity);
+    } else {
+      res.render("create-item-page", {
+        categories: categoryNames,
+        errors: [{ msg: "Item  already exists in the category" }],
+      }); // if exists, alert user
+      return;
+    }
+    res.redirect(`/categories/${categoryId}`);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
   }
-  res.redirect(`/categories/${categoryId}`);
 }
+
 async function showItemInCategoryGet(req, res) {
-  const dataRows = await db.selectOrderByItemIdAndCategoryId(
-    req.params.itemId,
-    req.params.categoryId
-  );
-  const itemId = dataRows[0].item_id;
-  const categoryId = dataRows[0].category_id;
-  const quantity = dataRows[0].quantity;
-  const itemNameRows = await db.searchItemNameById(itemId);
-  const categoryNameRows = await db.searchCategoryNameById(categoryId);
-  const itemName = itemNameRows[0].item_name;
-  const categoryName = categoryNameRows[0].category_name;
-  const order = {
-    categoryId: categoryId,
-    itemId: itemId,
-    itemName: itemName,
-    categoryName: categoryName,
-    quantity: quantity,
-  };
-  res.render("item-view", { order: order });
+  try {
+    const dataRows = await db.selectOrderByItemIdAndCategoryId(
+      req.params.itemId,
+      req.params.categoryId
+    );
+    const itemId = dataRows[0].item_id;
+    const categoryId = dataRows[0].category_id;
+    const quantity = dataRows[0].quantity;
+    const itemNameRows = await db.searchItemNameById(itemId);
+    const categoryNameRows = await db.searchCategoryNameById(categoryId);
+    const itemName = itemNameRows[0].item_name;
+    const categoryName = categoryNameRows[0].category_name;
+    const order = {
+      categoryId: categoryId,
+      itemId: itemId,
+      itemName: itemName,
+      categoryName: categoryName,
+      quantity: quantity,
+    };
+    res.render("item-view", { order: order });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+  }
 }
+
 async function removeItemFromCategoryGet(req, res) {
-  await db.removeItemFromCategory(req.params.itemId, req.params.categoryId);
-  res.redirect(`/categories/${req.params.categoryId}`);
+  try {
+    await db.removeItemFromCategory(req.params.itemId, req.params.categoryId);
+    res.redirect(`/categories/${req.params.categoryId}`);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+  }
 }
+
 module.exports = {
   showCreateItemPageGet,
   validateItemForm,
